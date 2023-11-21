192ef16cda82e5423b73708fbbae9af5406ed898
Khagani007 <khaganimammadli1999@gmail.com>
2023-11-21 14:52:06 +0400

    commit

--- src/BodyMassIndexCalculation.java
+++ src/BodyMassIndexCalculation.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class BodyMassIndexCalculation {
+import java.util.Scanner;
+
+public class BodyMassIndexCalculation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your height in meters (like 1.72) : ");
+        double height = input.nextDouble();
+
+        System.out.print("Enter your weight in kg: ");
+        double weight = input.nextDouble();
+
+        double bmi = weight / (height * height);
+
+        System.out.println("Your BMI is: " + bmi);
+        if (bmi < 18.5) {
+            System.out.println("You are underweight");
+        } else if (bmi > 18.5 && bmi < 24.9) {
+            System.out.println("You are healthy");
+        } else if (bmi > 24.9 && bmi < 30) {
+            System.out.println("You are overweight");
+        } else if (bmi > 30) {
+            System.out.println("You are obese");
+        }
+    }
 }
